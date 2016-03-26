6d8bc3b3998dc7ebd43da0937e2fab7265b709f7
Eric Gang <gz120748@gmail.com>
2016-03-26 15:13:58 +0800

    Original Setup
    Without android

---
+++ Samurai/desktop/src/com/samurai/el/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.samurai.el.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.samurai.el.Samurai;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Samurai(), config);
+	}
+}
