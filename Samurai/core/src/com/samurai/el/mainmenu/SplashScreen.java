3fae50beefb4c2f1485df55d20450ae54f446eba
HikariChao <gz120748@gmail.com>
2016-05-26 23:44:27 +0800

    SplashScreen v2

--- Samurai/core/src/com/samurai/el/mainmenu/SplashScreen.java
+++ Samurai/core/src/com/samurai/el/mainmenu/SplashScreen.java
@@ -2,29 +2,28 @@ package com.samurai.el.mainmenu;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class SplashScreen implements Screen, InputProcessor{
-	public Sprite[] frames;
+	public Texture[] frames;
 	public int count;
 	public float frameLatency;
 	public SpriteBatch batch;
 	public Game game;
+	public Music bgm;
 	
 	public SplashScreen(Game game) {
 		this.game = game;
-		frames = new Sprite[76];
-		for(int i = 0 ; i < 76; i++) {
-			frames[i] = new Sprite(new Texture(Gdx.files.internal("img/splash/01"+i+".jpg")));
-		}
+		frames = new Texture[264];
+		bgm = Gdx.audio.newMusic(Gdx.files.internal("music/bgm0.mp3"));
+		
 		count = 0;
 		batch = new SpriteBatch();
-		frameLatency = 0.05f;
+		frameLatency = 0.001f;
 	}
 
 	@Override
@@ -34,98 +33,107 @@ public class SplashScreen implements Screen, InputProcessor{
 
 	@Override
 	public void render(float delta) {
-		if(count >= 76) {
+		if(count >= 264) {
+			bgm.stop();
 			ScreenCenter.setscreencenter(game);
 			game.setScreen(new MainMenuScreen());
-			System.out.println("set");
+			dispose();
 		} else {
+			if(count < 10) {
+				frames[count] = new Texture(Gdx.files.internal("img/splash/00"+count+".jpg"));
+			}
+			else if(count < 100) {
+				frames[count] = new Texture(Gdx.files.internal("img/splash/0"+count+".jpg"));
+			}
+			else {
+				frames[count] = new Texture(Gdx.files.internal("img/splash/"+count+".jpg"));
+			}
 			frameLatency -= Gdx.graphics.getDeltaTime();
 			batch.begin();
-			frames[count].draw(batch);
+			batch.draw(frames[count], 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 			batch.end();
+			if(count == 12) {
+				bgm.play();
+			}
 			if(frameLatency <= 0) {
 				count += 1;
-				frameLatency = 0.05f;
+				frameLatency = 0.001f;
 			}
 		}
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-		
+		for(int i = 0; i < 264; i++) {
+			if(frames[i] == null) break;
+			frames[i].dispose();
+		}
+		bgm.dispose();
 	}
 
 	@Override
 	public boolean keyDown(int keycode) {
 		ScreenCenter.setscreencenter(game);
 		game.setScreen(new MainMenuScreen());
+		dispose();
 		return false;
 	}
 
 	@Override
 	public boolean keyUp(int keycode) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean keyTyped(char character) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
+		ScreenCenter.setscreencenter(game);
+		game.setScreen(new MainMenuScreen());
+		dispose();
 		return false;
 	}
 
 	@Override
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean mouseMoved(int screenX, int screenY) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean scrolled(int amount) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 	
